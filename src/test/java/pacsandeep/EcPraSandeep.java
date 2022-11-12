dbed66728ff2b22e3d2cb219fd3d2d6ff0f5d4da
bhukya <janardhanbhukya002@gmail.com>
2022-11-12 12:01:36 +0530

    Cpommit by Branch sand

---
+++ src/test/java/pacsandeep/EcPraSandeep.java
@@ -0,0 +1,10 @@
+package pacsandeep;
+
+public class EcPraSandeep {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Branch practice by sandeep");
+	}
+
+}
