25491aff36c032b4c4deef2552b5b7ac5411d1b4
bhukya <janardhanbhukya002@gmail.com>
2022-11-12 12:19:50 +0530

    hi

---
+++ gitbashPro1.java
@@ -0,0 +1,10 @@
+package GitBash;
+
+public class gitbashPro1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("GitBash to push to repo");
+	}
+
+}
