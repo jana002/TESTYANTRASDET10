d640b558538204f7ca5d08dde3ac54b7a3159679
bhukya <janardhanbhukya002@gmail.com>
2022-11-12 12:09:50 +0530

    commited by sandeep programing

---
+++ src/test/java/pacsandeep/EcPraSandeep2.java
@@ -0,0 +1,11 @@
+package pacsandeep;
+
+public class EcPraSandeep2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("sandeep programing 2");
+
+	}
+
+}
